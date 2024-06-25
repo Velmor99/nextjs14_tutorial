851df240e98e205ece67a80188e23aaffa21834b
Velmor99 <wincentwangog@gmail.com>
2024-06-25 23:25:49 +0300

    Parallel routes and slots

---
+++ src/app/complex-dashboard/@users/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Card from "../../../components/card";
+
+export default function UserAnalytics() {
+  return (
+    <Card>
+      <h1>User analytics</h1>
+    </Card>
+  );
+}
---
+++ src/app/complex-dashboard/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ComlexDashboardPage() {
+  return <h1>Complex dashboard</h1>;
+}
