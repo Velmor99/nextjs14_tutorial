f1ccd624f9dcae56700683d9e0edc29460f0848f
Velmor99 <wincentwangog@gmail.com>
2024-06-25 22:43:50 +0300

    done error handling

--- src/app/products/[productId]/layout.tsx
+++ src/app/products/[productId]/layout.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
+const getRandomInt = (number: number) => {
+  return Math.floor(Math.random() * number);
+};
+
 export default function ProductDetailsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error loading product");
+  }
+
   return (
     <>
       {children}
